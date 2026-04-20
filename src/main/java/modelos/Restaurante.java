c8a3edb8ed6347a6a9615432f8fc5037c7232446
ssimofatine <simohaji147@gmail.com>
2026-04-20 17:00:56 +0200

    calse Restaurante Termina

---
+++ src/main/java/modelos/Restaurante.java
@@ -0,0 +1,89 @@
+package modelos;
+
+import java.util.ArrayList;
+import java.util.List;
+import lombok.*;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+
+public class Restaurante {
+    //Atributos
+    //String nombre
+    //List<Cliente> clientes
+    //List<Reserva> reservas
+
+
+    private String nombre;
+    private List<Cliente> clientes;
+    private List<Reserva> reservas;
+
+    public Restaurante(String nombre) {
+        this.nombre = nombre;
+        this.clientes = new ArrayList<>();
+        this.reservas = new ArrayList<>();
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+
+    public List<Cliente> getClientes() {
+        return clientes;
+    }
+
+    public List<Reserva> getReservas() {
+        return reservas;
+    }
+
+    //addCliente(Cliente c)
+
+    public void addCliente(Cliente c){
+
+        this.clientes.add(c);
+
+
+    }
+
+
+    //addReserva(Reserva r)
+
+    public void addReserva(Reserva r) {
+
+        this.reservas.add(r);
+    }
+
+
+
+    //getClientePorDni(String dni)
+    public void getClientePorDni(String dni){
+        for (Cliente cliente : clientes){
+            if (cliente.getDni().equals(dni)){
+                System.out.println(cliente);
+            }
+        }
+    }
+    //getReservas()
+
+    public Reserva getReservas(){
+        for (Reserva reserva : reservas){
+            System.out.println(reserva);
+            return reserva;
+        }
+        return null;
+    }
+    //toString() que muestre los clientes y reservas almacenados.
+
+
+    @Override
+    public String toString() {
+        final StringBuffer sb = new StringBuffer("Restaurante{");
+        sb.append("nombre='").append(nombre).append('\'');
+        sb.append(", clientes=").append(clientes);
+        sb.append(", reservas=").append(reservas);
+        sb.append('}');
+        return sb.toString();
+    }
+}
