524e00f0a1475ce9e7ad2be7724dc10c5a7898a7
Mohamed El Fatine <simohaji147@gmail.com>
2026-04-21 00:40:19 +0200

    crear calse RestauranteService

--- src/main/java/servicios/RestauranteService.java
+++ src/main/java/servicios/RestauranteService.java
@@ -1,7 +1,9 @@
 package servicios;
+// yo uso metodo como eso (*) por importe todo
 import modelos.*;
 import java.util.*;
 import Enums.*;
+import java.time.*;
 
 public class RestauranteService {
 }
