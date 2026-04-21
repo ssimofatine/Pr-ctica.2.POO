a1a4529039049111bdedbaa663d1a8b50564ed7f
Mohamed El Fatine <simohaji147@gmail.com>
2026-04-21 02:24:40 +0200

    crear calse RestauranteService

--- src/main/java/servicios/RestauranteService.java
+++ src/main/java/servicios/RestauranteService.java
@@ -4,6 +4,24 @@ import modelos.*;
 import java.util.*;
 import Enums.*;
 import java.time.*;
+import java.util.stream.Collectors;
 
 public class RestauranteService {
+    // llama calse restaurante privada
+    private Restaurante restaurante;
+
+    // constructor
+
+    public RestauranteService(Restaurante restaurante) {
+        this.restaurante = restaurante;
+    }
+
+    public List<Reserva> getReservasConfirmadas(LocalDate fecha){
+        //mostrar todas las reservas con estado CONFIRMADA
+        //de una fecha dada, ordenadas por hora de forma ascendente.
+        return restaurante.getReservas().stream()
+                .filter(r -> r.getEstado() == EstadoReserva.CONFIRMADA && r.getFecha().equals(fecha))
+                .sorted()
+                .collect(Collectors.toList());
+    }
 }
